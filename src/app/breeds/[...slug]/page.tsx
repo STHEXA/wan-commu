5ce0b974e2c48cce90d998cbf29a9c569b8d8796
STHEXA <ti.syu.s@gmail.com>
2025-11-16 10:05:50 +0900

    不要な記述削除

--- src/app/breeds/[...slug]/page.tsx
+++ src/app/breeds/[...slug]/page.tsx
@@ -1,21 +1,5 @@
 import DogImage from "@/components/DogImage";
-import { AllDogs } from "@/type/dog";
 import { replaceSlash } from "@/utils/replaceSlash";
-
-// export async function generateStaticParams() {
-//   const res = await fetch("https://dog.ceo/api/breeds/list/all");
-//   const data: AllDogs = await res.json();
-
-//   const breeds = data.message;
-
-//   const paths = Object.entries(breeds).flatMap(([breed, subBreeds]) =>
-//     subBreeds.length === 0
-//       ? [{ slug: [breed] }]
-//       : subBreeds.map((sub: string) => ({ slug: [breed, sub] }))
-//   );
-
-//   return paths;
-// }
 
 type BreedPageProps = {
   params: Promise<{
